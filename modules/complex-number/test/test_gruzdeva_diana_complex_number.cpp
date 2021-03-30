f39f7fe16b43de8ea70e914a49cf7b59d80690c3
Diana Gruzdeva <taciturn.homebody@gmail.com>
2021-03-30 14:56:06 +0300

    change test 'Long_equation', fix copyright

--- modules/complex-number/test/test_gruzdeva_diana_complex_number.cpp
+++ modules/complex-number/test/test_gruzdeva_diana_complex_number.cpp
@@ -1,31 +1,34 @@
-// Copyright 2021 Diana Gruzdeva
+// Copyright 2021 Gruzdeva Diana
 
 #include <gtest/gtest.h>
 
 #include <string>
+#include <limits>
+#include <iostream>
 
 #include "include/complex_number.h"
 
 TEST(Gruzdeva_Diana_ComplexNumberTest, Long_equation) {
-  double resultRe = 2.0;
-  double resultIm = 19.0;
-  double re1 = 5.0;
-  double im1 = 4.0;
-  double re2 = 25.0;
-  double im2 = -3.0;
-  double re3 = 12.0;
-  double im3 = -7.0;
-  double re4 = -6.0;
-  double im4 = -9.0;
+  double resultRe = 4.2030000000000029;
+  double resultIm = 16.7734000000000093;
+  double re1 = 5.24;
+  double im1 = 4.53;
+  double re2 = 25.82;
+  double im2 = -3.26;
+  double re3 = 12.72;
+  double im3 = -7.04;
+  double re4 = -6.01;
+  double im4 = -9.86;
+  double error = std::numeric_limits<double>::epsilon();
 
   ComplexNumber z1(re1, im1);
   ComplexNumber z2(re2, im2);
   ComplexNumber z3(re3, im3);
   ComplexNumber z4(re4, im4);
   ComplexNumber zReal = z1 * z2 + z3 * z4;
-
-  ASSERT_EQ(resultRe, zReal.getRe());
-  ASSERT_EQ(resultIm, zReal.getIm());
+  std::cout << std::fixed << std::setprecision(25) << zReal.getIm();
+  ASSERT_NEAR(resultRe, zReal.getRe(), error);
+  ASSERT_NEAR(resultIm, zReal.getIm(), error);
 }
 
 TEST(Gruzdeva_Diana_ComplexNumberTest, Strings) {
